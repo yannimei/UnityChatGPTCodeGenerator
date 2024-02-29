2c6e8921d0020325c8ce931090a5ddba6d856959
yannimei <mei_yanni@163.com>
2024-02-29 17:25:38 +0100

    add Core file and GPTSetting

---
+++ Assets/Scripts/ChatGPT/ChatGTPSetting.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="ChatGPTSetting", menuName ="ChatGPT/ChatGPTSettings")]
+public class ChatGTPSetting : ScriptableObject
+{
+    public string apiURL;
+
+    public string apiKey;
+
+    public bool debug;
+
+    public string[] reminders;
+}
