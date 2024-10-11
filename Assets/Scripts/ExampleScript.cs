284693ffcc146696fd227a87f587361fe403682f
yannimei <mei_yanni@163.com>
2024-10-11 15:11:42 +0200

    enable parameter editting

--- Assets/Scripts/ExampleScript.cs
+++ Assets/Scripts/ExampleScript.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class ExampleScript : MonoBehaviour, ChatGPTScript
 {
+    public float speed;
+    public void SetSpeed(float speed) 
+    { 
+        this.speed = speed;
+    }
+
+    public float GetSpeed() 
+    {
+        return speed;
+    }
+
     public void DestroyScript()
     {
         Destroy(this);
@@ -17,6 +28,11 @@ public class ExampleScript : MonoBehaviour, ChatGPTScript
     public string GetName()
     {
         return "scriptname";
+    }
+
+    public ScriptParamData[] GetParams()
+    {
+        return new ScriptParamData[] {new ScriptParamData("speed", this.GetSpeed, this.SetSpeed)};
     }
 
     public void SetActive(bool active)
--- Assets/Scripts/ScriptManager.cs
+++ Assets/Scripts/ScriptManager.cs
@@ -5,8 +5,24 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
+public struct ScriptParamData
+{
+    public string name;
+    public Func<float> Get;
+    public Action<float> Set;
+    // TODO: add reset method?
+
+    public ScriptParamData(string name, Func<float> Get, Action<float> Set)
+    {
+        this.name = name;
+        this.Get = Get;
+        this.Set = Set;
+    }
+}
+
 public interface ChatGPTScript
 {
+    public ScriptParamData[] GetParams();
     public string GetName();
     public void DestroyScript();
     // TODO: add method to duplicate the script?
@@ -22,9 +38,15 @@ public class ScriptManager : MonoBehaviour
     private List<ChatGPTScript> scripts = new List<ChatGPTScript>();
     private List<string> prompts = new List<string>();
     private int currentScriptIndex;
+
+    private ScriptParamData[] paramData;
+    private int currentParamIndex;
+
     [SerializeField] private TMP_Text scriptLabel;
     [SerializeField] private Text prompt;
     [SerializeField] private TMP_Text indexIndicator;
+    [SerializeField] private TMP_Text paraName;
+    [SerializeField] private TMP_Text paraValue;
 
     // called by button on the UI
     public void DestroyCurrentScript()
@@ -32,6 +54,13 @@ public class ScriptManager : MonoBehaviour
         ChatGPTScript script = this.scripts[this.currentScriptIndex];
         this.scripts.Remove(script);
         script.DestroyScript();
+        // TODO: update UI
+       if (currentScriptIndex >= scripts.Count)
+        {
+            currentScriptIndex = scripts.Count - 1;
+        }
+        ShowScript(currentScriptIndex);
+        ShowParam(0);
     }
 
     public void DisableCurrentScript()
@@ -56,12 +85,42 @@ public class ScriptManager : MonoBehaviour
         if (this.currentScriptIndex > 0) this.ShowScript(this.currentScriptIndex - 1);
     }
 
-    private void ShowScript(int index)
+    private void ShowScript(int index, bool forceParamUpdate=false)
     {
+        if (index < 0) 
+        {
+            currentScriptIndex = 0;
+            scriptLabel.text = "";
+            indexIndicator.text = $"0/0";
+            return;
+        }
+
+        bool changed = index != currentScriptIndex;
         currentScriptIndex = index;
         scriptLabel.text = prompts[index];
-        // TODO: update UI to show name of the new script param (check if the script has at least one param)
-        indexIndicator.text = $"{index+1}/{scripts.Count}";
+        indexIndicator.text = $"{index + 1}/{scripts.Count}";
+        // update UI to show name of the new script param (check if the script has at least one param)
+        if (changed || forceParamUpdate)
+        {
+            paramData = scripts[currentScriptIndex].GetParams(); // update the parameter list for a new script
+            // reset the parameter index to 0 when switching to a new script
+            ShowParam(0);
+        }
+    }
+
+    private void ShowParam(int index)
+    {
+        currentParamIndex = index;
+        if (paramData.Length >= index)
+        {
+            paraName.text = paramData[currentParamIndex].name;
+            paraValue.text = paramData[currentParamIndex].Get().ToString();
+        }
+        else
+        {
+            paraName.text = "";
+            paraValue.text = "";
+        }
     }
 
     public void AddScript(ChatGPTScript newScript)
@@ -69,6 +128,26 @@ public class ScriptManager : MonoBehaviour
         scripts.Add(newScript);
         prompts.Add(prompt.text);
 
-        ShowScript(currentScriptIndex);
+        ShowScript(currentScriptIndex, true);
+        ShowParam(currentParamIndex);
+    }
+
+    // called by different buttons on the UI
+    public void ChangeCurrentParam(float delta)
+    {
+        float current = this.paramData[this.currentParamIndex].Get();
+        this.paramData[this.currentParamIndex].Set(current + delta);
+        ShowParam(currentParamIndex);
+    }
+
+    public void ShowNextParam()
+    {
+        if (this.currentParamIndex + 1 < this.paramData.Length) this.ShowParam(this.currentParamIndex + 1);
+    }
+
+    // called by button on the UI
+    public void ShowPreviousParam()
+    {
+        if (this.currentParamIndex > 0) this.ShowParam(this.currentParamIndex - 1);
     }
 }
